6a0ae264385d4a4041f6302522d7812c82040dc8
User -2023BBYXF\Administrator <jin.harry97@gmail.com>
2023-09-04 17:58:47 +0900

    9/4 - 숙제

---
+++ MyWork/Java01/src/a01_homework/Ex03_230904.java
@@ -0,0 +1,19 @@
+package a01_homework;
+
+// 6-1
+class Student {
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+}
+
+public class Ex03_230904 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
