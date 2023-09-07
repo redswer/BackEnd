eb8542e6f94dd10d4a803757382a08e8f6776f36
User -2023BBYXF\Administrator <jin.harry97@gmail.com>
2023-09-07 15:28:20 +0900

    9/7 - 상속

--- MyWork/Java01/src/j07_classExtends/Ex02_CarTest.java
+++ MyWork/Java01/src/j07_classExtends/Ex02_CarTest.java
@@ -23,6 +23,7 @@ package j07_classExtends;
 
 class SportsCar extends Ex00_Car {
 	int turbo;
+	int speed;
 
 	SportsCar() {
 		super(); // => 작성하지 않아도 컴파일러가 자동으로 넣어줌 -> 조상의 default 생성자를 의미
@@ -64,9 +65,45 @@ class SportsCar extends Ex00_Car {
 		return this.turbo += turbo;
 	}
 
+//	=================================================
+	// 조상과 동일한 이름의 메서드 -> 메서드 오버라이딩
+	// => 조상(super) / 자식(this) 구별 필요(멤버변수에도 동일하게 적용)
+	// => 조상에 정의된 메서드의 기능을 업그레이드 하기 위해 주로 사용
+
+	@Override // => 오버라이딩 표시
+	public void speedUp() {
+		super.speedUp();
+		this.speed += 100;
+	}
+
+	// @Override // => 메서드 오버라이딩 아니고 메서드 오버로드 이므로 오류
+	public void speedUp(int speed) {
+		this.speed += speed;
+		super.speed += speed;
+		System.out.printf("speed = %d, this.speed = %d, super.speed = %d, COUNTRY = %s", speed, this.speed, super.speed,
+				Ex00_Car.COUNTRY);
+		// => static 한 변수는 상속에 관계없이 static 하게 사용
+		// => 따라서 super.COUNTRY 가 아닌 Ex00_Car.COUNTRY
+	}
+
+//	===============
+	// final 선언한 메서드 오버라이드 test
+
+//	public void speedDown() {
+//		speed -= 100;
+//	}
+	// => 오버라이딩 불가능
+
+	public void speedDown(int i) {
+		speed -= 100;
+	}
+	// => 매개변수가 다르므로 오버로딩 -> 오버로딩은 허용
+
+//	===================================================
+
+	@Override
 	public String toString() {
-		return "Car [ speed : " + super.speed + ", mileage : " + super.mileage + ", color : " + super.color + " ]\n"
-				+ "SportsCar [ turbo : " + turbo + " ]";
+		return super.toString() + "\n" + "SportsCar [ turbo : " + turbo + " ]";
 	}
 
 } // SportsCar
@@ -92,7 +129,7 @@ public class Ex02_CarTest {
 		// => 조상 생성자 -> 자식 생성자 순으로 실행
 
 //		==================================================
-		// ** 생성자 test
+		// 2) 생성자 test
 
 		SportsCar sc2 = new SportsCar(100);
 		System.out.println("** 생성자 test : " + sc2 + "\n");
@@ -100,13 +137,32 @@ public class Ex02_CarTest {
 		// => default 생성자(Ex00_Car())가 없으면 자식 클래스(SportsCar)를 생성할 수 없음
 
 //		==================================================
-		// ** super() (조상의 생성자 호출) / super. (생성자 추가하지 않고 초기화) Test
+		// 3) super() (조상의 생성자 호출) / super. (생성자 추가하지 않고 초기화) Test
 
 		SportsCar sc3 = new SportsCar(300, 1000, 2000, "Red");
 		System.out.println("super() test : " + sc3 + "\n");
 
 		SportsCar sc4 = new SportsCar(200, 1500, 2000);
-		System.out.println("super. test : " + sc4);
+		System.out.println("super. test : " + sc4 + "\n");
+
+//		=====================================================
+		// 4) 멤버 변수 test
+
+		System.out.println("자식 클래스에 speed 추가 test : " + sc4.speed);
+		// => SportsCar (자식 클래스)에 int speed 추가하기 전
+		// => Car (부모 클래스)의 speed 값 출력
+//		====================
+		// => SpotsCar (자식 클래스)에 int speed 추가 후
+		// => SportsCar (자식 클래스)의 speed 값 출력
+
+//		====================================================
+		// 5) 메서드 test
+		// => 부모 클래스의 메서드와 동일한 이름의 메서드가 있을 때
+
+		sc4.speedUp();
+		System.out.println("동일한 이름의 메서드 추가 후 speed : " + sc4.speed + "\n");
+
+		sc4.speedUp(20);
 
 	} // main
 
