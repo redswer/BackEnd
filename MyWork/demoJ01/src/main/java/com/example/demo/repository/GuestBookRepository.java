5ced13c649f05dba3dfa42abeebd0456c745b668
User -2023BBYXF\Administrator <jin.harry97@gmail.com>
2023-11-20 10:20:51 +0900

    11/17

--- MyWork/demoJ01/src/main/java/com/example/demo/repository/GuestBookRepository.java
+++ MyWork/demoJ01/src/main/java/com/example/demo/repository/GuestBookRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.GuestBook;
 
+//** interface JpaRepository<T, ID>
+//=> interface 계층도
+//	Repository<T, ID> -> CrudRepository<T, ID> 
+//	-> PagingAndSortingRepository<T, ID> -> JpaRepository<T, ID>
+
+//=> CrudRepository<T, ID> : 기본기능만 사용하는 경우 상속받음
+//=> JpaRepository<T, ID> : JPA 관련기능 대부분 사용하는경우 상속받음
+
+//=> 이들을 상속받는것만으로도 모든 처리가 됨.
+//=> Test Code 로 확인가능
+
 public interface GuestBookRepository 
 					extends JpaRepository<GuestBook, Long> {
 }
